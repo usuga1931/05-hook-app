93b5508aeb384b64d5ff4474b17b19ba522cf135
Juan Usuga <juan.usuga@integ.ro>
2021-03-13 19:41:56 -0500

    Hook useContext

---
+++ src/components/09-useContext/LoginScreen.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
+
+export const LoginScreen = () => {
+
+    const { setUser } = useContext(UserContext);
+
+    return (
+        <div>
+            <h1>LoginScreen</h1>
+            <hr />
+            <button
+                className="btn btn-primary"
+                onClick={() => setUser({
+                    id: 1,
+                    name: 'Juan Usuga'
+                })}
+            >
+                Login
+            </button>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 
 ReactDOM.render(
-    <TodoApp />,
+    <MainApp />,
     document.getElementById('root')
 );
